9dcbfc57a19ce47afd6f2d5621844aa6843f3204
Raymark Pena <dev.mlimp@gmail.com>
2026-02-26 14:14:35 +0800

    fix: use default Sonner colors instead of richColors

--- resources/js/layouts/app-layout.tsx
+++ resources/js/layouts/app-layout.tsx
@@ -47,7 +47,7 @@ export default function AppLayout({ children, breadcrumb }: AppLayoutProps) {
                     <div className="flex flex-1 flex-col gap-4 p-4 pt-0">{children}</div>
                 </SidebarInset>
             </SidebarProvider>
-            <Toaster richColors position="top-right" />
+            <Toaster position="top-right" />
         </TooltipProvider>
     );
 }
